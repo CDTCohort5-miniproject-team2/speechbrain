24ce407c2ad65524b30e3cbe615653bdf4460dda
jasonchanly <JLYChan1@sheffield.ac.uk>
2024-01-11 08:37:25 +0200

    Updated basic gpu test

--- TEAM2_modifications/_TEAM2_speechbrain_gpu_setup_test.py
+++ TEAM2_modifications/_TEAM2_speechbrain_gpu_setup_test.py
@@ -1,7 +1,7 @@
 import torch
 
-print(torch.cuda.is_available())
-print(torch.cuda.current_device())
-print(torch.cuda.device(0))
-print(torch.cuda.device_count())
-print(torch.cuda.get_device_name(0))
+print("CUDA is available:", torch.cuda.is_available())
+print("Current device:", torch.cuda.current_device())
+print("Default device ref:", torch.cuda.device(0))
+print("No. of GPUs:", torch.cuda.device_count())
+print("Name of default device:", torch.cuda.get_device_name(0))
