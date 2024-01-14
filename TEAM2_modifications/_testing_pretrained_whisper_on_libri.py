fba7178eaaa880537ba48f6ae995ae29f9a8515c
jasonchanly <JLYChan1@sheffield.ac.uk>
2024-01-14 15:08:46 +0000

    Update _testing_pretrained_whisper_on_libri.py

--- TEAM2_modifications/_testing_pretrained_whisper_on_libri.py
+++ TEAM2_modifications/_testing_pretrained_whisper_on_libri.py
@@ -1,15 +1,19 @@
-"""Copied from https://huggingface.co/openai/whisper-tiny.en to validate wer obtained"""
+"""Adapted from https://huggingface.co/openai/whisper-tiny.en to validate wer obtained
+- running on local computer instead of HPC due to storage constraints
+RESULT: 5.655609406528749"""
 
 from datasets import load_dataset
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 import torch
 from evaluate import load
 
-# TEAM2: this takes up too much memory in home space, to implement workaround
-# librispeech_test_clean = load_dataset("librispeech_asr", "clean", split="test")
+# TODO: TEAM2 - do not run this as-is on HPC, storing dataset takes up too much memory in home space, to implement workaround
+librispeech_test_clean = load_dataset("librispeech_asr", "clean", split="test")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 processor = WhisperProcessor.from_pretrained("openai/whisper-tiny.en")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en").to("cuda")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en").to(device)
 
 def map_to_pred(batch):
     audio = batch["audio"]
@@ -17,7 +21,7 @@ def map_to_pred(batch):
     batch["reference"] = processor.tokenizer._normalize(batch['text'])
 
     with torch.no_grad():
-        predicted_ids = model.generate(input_features.to("cuda"))[0]
+        predicted_ids = model.generate(input_features.to(device))[0]
     transcription = processor.decode(predicted_ids)
     batch["prediction"] = processor.tokenizer._normalize(transcription)
     return batch
@@ -26,3 +30,4 @@ result = librispeech_test_clean.map(map_to_pred)
 
 wer = load("wer")
 print(100 * wer.compute(references=result["reference"], predictions=result["prediction"]))
+# >>> 5.655609406528749
