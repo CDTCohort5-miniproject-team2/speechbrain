21f8482d754b8ebf218cf6d85255303c38d57dd5
Paul Gering <pablogering@gmail.com>
2024-03-15 09:42:26 +0000

    calculation of confidence intervals

--- TEAM2_modifications/paul_testing_CI.py
+++ TEAM2_modifications/paul_testing_CI.py
@@ -1,16 +1,33 @@
 # pip install confidence_intervals
 
 from confidence_intervals import evaluate_with_conf_int
+import sklearn.metrics
+import pandas as pd
+import numpy as np
 
-# Define the metric of interest (could be a custom method)
-from sklearn.metrics import accuracy_score
+wer_df = pd.read_csv("./baseline_w_whisper_large_results.csv")
 
-# Need to create an array with the WER per transcript
-samples = None
+conditions = np.array(wer_df['condition'])
+wer_array = np.array(wer_df['merged_wer'])
+
+#condition_wer_df = wer_df[['condition', 'merged_wer']]
+
+
+#condition_1_wer = wer_df[wer_df['condition'] == 1]['merged_wer'].values
+#condition_2_wer = wer_df[wer_df['condition'] == 2]['merged_wer'].values
+
+#WER_per_condition_array = np.column_stack((condition_1_wer, condition_2_wer))
+
+alpha = 5
+num_bootstraps = int(50/alpha*100)
+
+def metric(wer):
+    return np.average(wer)
+
 # Run the function. In this case, the samples are represented by the categorical decisions
 # made by the system which, along with the labels, is all that is needed to compute the metric.
 
-output = evaluate_with_conf_int(samples=None, metric = accuracy_score, conditions = , num_bootstraps=1000, alpha=5)
+output = evaluate_with_conf_int(samples=wer_array, metric = metric, labels = None, conditions = conditions, num_bootstraps=num_bootstraps, alpha=alpha, samples2 = None)
 print(output)
 
 # First number if the metric value on the full dataset. The list indicates the lower and upper bound of the confidence interval
