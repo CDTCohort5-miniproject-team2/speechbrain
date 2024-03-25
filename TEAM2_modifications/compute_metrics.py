0cdc7c0f239ffed31e0260062415282635047e21
jasonchanly <JLYChan1@sheffield.ac.uk>
2024-03-25 12:37:48 +0000

    Updated metrics computing to use a different library

--- TEAM2_modifications/compute_metrics.py
+++ TEAM2_modifications/compute_metrics.py
@@ -1,9 +1,7 @@
 import collections
 import numpy as np
-import pesq.cypesq
 from scipy import signal
 from scipy.io import wavfile
-import pysepm
 from pathlib import Path
 from speechbrain.utils.edit_distance import accumulatable_wer_stats
 import TEAM2_utils
@@ -11,11 +9,14 @@ import pandas as pd
 import librosa
 import warnings
 from numba import NumbaDeprecationWarning
+
 warnings.filterwarnings("ignore", category=NumbaDeprecationWarning)
 from tqdm import tqdm
 import re
+from pb_bss_eval.evaluation import pesq, stoi  # FYI you also need to pip install cython
 
 SAMPLE_RATE = 16000
+
 
 class ExperimentResults:
     def __init__(self, rq, data_csv_fpath, set_split="Training", data_directory="kroto_data", save_as_csv=True):
@@ -32,14 +33,15 @@ class ExperimentResults:
         self.save_as_csv = save_as_csv
         self.designation = f"{TEAM2_utils.EXPERIMENT_DESIGNATION[self.rq]}_w_medium_en"
         self.parent_output_dir = Path(f"kroto_data/{self.designation}")
-        self.save_csv_fpath = self.parent_output_dir/f"{self.designation}_results.csv"
+        self.save_csv_fpath = self.parent_output_dir / f"{self.designation}_results.csv"
         self.output_dirs = [Path(f"kroto_data/{self.designation}/{subdir}") for subdir in TEAM2_utils.EXPERIMENT_OUTPUT_DIRS]
         self.merged_pred_nlp_dir, self.merged_pred_wer_dir, self.customer_pred_txt_dir, \
-            self.customer_pred_wav_dir, self.server_pred_txt_dir, self.server_pred_wav_dir = self.output_dirs
+        self.customer_pred_wav_dir, self.server_pred_txt_dir, self.server_pred_wav_dir = self.output_dirs
         self._check_output_dirs()
 
         self.original_df = pd.read_csv(data_csv_fpath)
-        self.original_df["scenario_id"] = [fname.replace(".wav", "").replace("16k_", "") for fname in self.original_df["Recording File reference"]]
+        self.original_df["scenario_id"] = [fname.replace(".wav", "").replace("16k_", "") for fname in
+                                           self.original_df["Recording File reference"]]
 
     def _check_output_dirs(self):
         if not self.parent_output_dir.exists():
@@ -82,23 +84,19 @@ class ExperimentResults:
 
     def compute_metrics(self):
 
-        new_df_col_names = ["pesq", "stoi", "composite_score_sig", "composite_score_bak", "composite_score_ovl",
-                            "pesq_v_wall_mic", "stoi_v_wall_mic",
-                            "composite_score_v_wall_mic_sig", "composite_score_v_wall_mic_bak",
-                            "composite_score_v_wall_mic_ovl",
+        new_df_col_names = ["pesq", "stoi",
                             "wer", "num_tokens_in_gt_transcript"]
 
         if self.rq == "baseline":
-            new_df_col_names += ["server_pesq", "server_stoi", "server_composite_score_sig", "server_composite_score_bak",
-                                 "server_composite_score_ovl", "server_wer",
-                                 "server_num_tokens_in_gt_transcript", "merged_wer", "merged_num_tokens_in_gt_transcript"]
+            new_df_col_names += ["server_wer", "server_num_tokens_in_gt_transcript", "merged_wer", "merged_num_tokens_in_gt_transcript"]
 
         for col_name in new_df_col_names:
             self.original_df[col_name] = np.nan
 
         self.original_df["already_parsed"] = False
 
-        filter_by_columns = ["already_parsed", "scenario_id", "Set", "has_noise", "num_passengers", "audio_duration_in_s"] + new_df_col_names
+        filter_by_columns = ["already_parsed", "scenario_id", "Set", "has_noise", "num_passengers",
+                             "audio_duration_in_s"] + new_df_col_names
         if self.save_csv_fpath.exists():
             print("Continuing from previous results.")
             self.results_df = pd.read_csv(self.save_csv_fpath)
@@ -119,10 +117,15 @@ class ExperimentResults:
                 print(self.results_df.at[i, "already_parsed"])
                 print(f"Skipping no. {i} - already parsed")
                 continue
+            # elif i in [210]:
+            #     # 20240207_113810_scenario_18,
+            #     print("Known problem with signal - skipped")
+            #     continue
             else:
+
                 merged_pred_nlp_fpath, merged_pred_wer_fpath, customer_pred_txt_fpath, \
                 customer_pred_wav_fpath, server_pred_txt_fpath, server_pred_wav_fpath = \
-                    [subdir/f"{scenario_id}_{suf}"
+                    [subdir / f"{scenario_id}_{suf}"
                      for subdir, suf in zip(self.output_dirs, TEAM2_utils.EXPERIMENT_FILEPATH_SUFFIXES)]
 
                 _, _, customer_gt_wall_mic_wav_fpath, \
@@ -138,43 +141,33 @@ class ExperimentResults:
                 if not customer_pred_wav_fpath.exists():
                     continue
                 else:
+                    print("Computing customer signal metrics")
                     try:
-                        c_pesq, c_stoi, c_compsig, c_compbak, c_compovl = \
+                        c_pesq, c_stoi = \
                             compute_signal_metrics(customer_gt_closetalk_wav_fpath, customer_pred_wav_fpath)
-                    except pesq.cypesq.NoUtterancesError:
+                    except:
                         print(f"No customer close talk signal for scenario: {scenario_id}")
-                        c_pesq, c_stoi, c_compsig, c_compbak, c_compovl = np.nan, np.nan, np.nan, np.nan, np.nan
-                    c_pesq_v_wall_mic, c_stoi_v_wall_mic, c_compsig_v_wall_mic, c_compbak_v_wall_mic, c_compovl_v_wall_mic = \
-                        compute_signal_metrics(customer_gt_wall_mic_wav_fpath, customer_pred_wav_fpath)
+                        c_pesq, c_stoi = np.nan, np.nan
 
-                    for score, col_name in zip([c_pesq, c_stoi, c_compsig, c_compbak, c_compovl,
-                                                c_pesq_v_wall_mic, c_stoi_v_wall_mic, c_compsig_v_wall_mic,
-                                                c_compbak_v_wall_mic, c_compovl_v_wall_mic],
-                                               ["pesq", "stoi", "composite_score_sig", "composite_score_bak",
-                                                "composite_score_ovl", "pesq_v_wall_mic", "stoi_v_wall_mic",
-                                                "composite_score_v_wall_mic_sig", "composite_score_v_wall_mic_bak",
-                                                "composite_score_v_wall_mic_ovl"]):
+                    for score, col_name in zip([c_pesq, c_stoi], ["pesq", "stoi"]):
                         self.results_df.at[i, col_name] = score
 
                     if not customer_gt_txt_fpath.exists():
+                        print("No customer ground truth transcript available.")
                         continue
                     else:
+                        print("Computing customer WER")
                         c_wer, c_num_tokens = compute_wer(customer_pred_txt_fpath, customer_gt_txt_fpath)
                         self.results_df.at[i, "wer"] = c_wer
                         self.results_df.at[i, "num_tokens_in_gt_transcript"] = c_num_tokens
 
                     if self.rq == "baseline":
-                        s_pesq, s_stoi, s_compsig, s_compbak, s_compovl = compute_signal_metrics(server_gt_wav_fpath,
-                                                                                                 server_pred_wav_fpath)
-
-                        for score, col_name in zip([s_pesq, s_stoi, s_compsig, s_compbak, s_compovl],
-                                                   ["server_pesq", "server_stoi", "server_composite_score_sig",
-                                                    "server_composite_score_bak", "server_composite_score_ovl"]):
-                            self.results_df.at[i, col_name] = score
 
                         if not server_gt_txt_fpath.exists():
+                            print("No server ground truth transcript available.")
                             continue
                         else:
+                            print("Computing server WER")
                             s_wer, s_num_tokens = compute_wer(server_pred_txt_fpath, server_gt_txt_fpath)
                             self.results_df.at[i, "server_wer"] = s_wer
                             self.results_df.at[i, "server_num_tokens_in_gt_transcript"] = s_num_tokens
@@ -182,16 +175,19 @@ class ExperimentResults:
                         if not merged_gt_fpath.exists():
                             continue
                         else:
+                            print("Computing merged WER")
                             m_wer, m_num_tokens = compute_wer(merged_pred_wer_fpath, merged_gt_fpath)
                             self.results_df.at[i, "merged_wer"] = m_wer
                             self.results_df.at[i, "merged_num_tokens_in_gt_transcript"] = m_num_tokens
-            if self.save_as_csv and (i % 3 == 0):
-                self.results_df.to_csv(self.save_csv_fpath, index=False, columns=filter_by_columns)
+
             self.results_df.at[i, "already_parsed"] = True
+            if self.save_as_csv:
+                self.results_df.to_csv(self.save_csv_fpath, index=False, columns=filter_by_columns)
 
         print(self.results_df.head())
         print(self.results_df.info())
         self.results_df.to_csv(self.save_csv_fpath, index=False, columns=filter_by_columns)
+
 
 def _synchronise_target_and_estimate(reference_audio, processed_audio, mode="simple_crop"):
     if mode == "simple_crop":
@@ -201,23 +197,24 @@ def _synchronise_target_and_estimate(reference_audio, processed_audio, mode="sim
             reference_audio = reference_audio[:-length_diff]
         elif length_diff < 0:
             print("Cropping processed audio for alignment")
-            processed_audio = processed_audio[:-length_diff]
+            processed_audio = processed_audio[:length_diff]
     else:
         raise NotImplementedError
 
     return reference_audio, processed_audio
+
 
 def compute_signal_metrics(reference_audio_fpath, processed_audio_fpath):
     reference_audio, sr = librosa.load(reference_audio_fpath, sr=None)
     processed_audio, sr = librosa.load(processed_audio_fpath, sr=None)
     reference_audio, processed_audio = _synchronise_target_and_estimate(reference_audio, processed_audio)
 
-    # TODO: Yao can you find an alternative github library that 
-    _, pesq_value = pysepm.pesq(reference_audio, processed_audio, SAMPLE_RATE)
-    stoi_value = pysepm.stoi(reference_audio, processed_audio, SAMPLE_RATE)
-    c_score_signal, c_score_background, c_score_overall = pysepm.composite(reference_audio, processed_audio, SAMPLE_RATE)
+    pesq_value = pesq(reference_audio, processed_audio, SAMPLE_RATE)
+    stoi_value = stoi(reference_audio, processed_audio, SAMPLE_RATE)
+    # NO LONGER USED c_score_signal, c_score_background, c_score_overall = pysepm.composite(reference_audio, processed_audio, SAMPLE_RATE)
 
-    return pesq_value, stoi_value, c_score_signal, c_score_background, c_score_overall
+    return pesq_value, stoi_value
+
 
 def compute_wer(predicted_fpath, ground_truth_fpath):
     # https: // speechbrain.readthedocs.io / en / latest / API / speechbrain.utils.edit_distance.html
@@ -235,11 +232,15 @@ def compute_wer(predicted_fpath, ground_truth_fpath):
     stats = accumulatable_wer_stats([ground_truth], [predicted], stats)
     return stats["WER"], stats["num_ref_tokens"]
 
-def main():
-    baseline_demo_results = ExperimentResults("baseline", "kroto_data/final_data_catalogue.csv",
-                                              set_split="Training", data_directory="kroto_data", save_as_csv=True)
 
-    baseline_demo_results.compute_metrics()
+def main():
+    for rq in ["adding_enhancer", "adding_separator", "enhancer_first", "separator_first"]:
+        print("Computing results for rq:", rq)
+        results = ExperimentResults(rq, "kroto_data/final_data_catalogue.csv",
+                                    set_split="Training", data_directory="kroto_data", save_as_csv=True)
+
+        results.compute_metrics()
+
 
 if __name__ == "__main__":
     main()
--- TEAM2_modifications/run_experiments.py
+++ TEAM2_modifications/run_experiments.py
@@ -3,8 +3,8 @@ import process_transcripts
 
 
 def main():
-    experiment = experiments.Experiment(rq="adding_enhancer",
-                                        data_csv_fpath="kroto_data/temporary_data_catalogue.csv",
+    experiment = experiments.Experiment(rq="adding_separator",
+                                        data_csv_fpath="kroto_data/final_data_catalogue.csv",
                                         set_split="Training",
                                         raw_data_directory="kroto_data",
                                         output_dir_suffix="w_medium_en",
